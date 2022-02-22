2e1d9d4ce91d34685f3e474c0afc041599295626
Juan Hdez <juanh7522@gmail.com>
2022-02-21 21:17:03 -0600

    inicial

---
+++ prueba.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  printf("Hello world!1");
+  printf("Hello world!2");
+  printf("Hello world!3");
+  int a[3][2] = {{5,8},{3,7},{5,10}};
+  printf("Hello world!4");
+  printf("Hello world!4");
+  
+  system("pause");
+  return 0;
+}
